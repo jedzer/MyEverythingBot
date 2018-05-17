6ff6ba033b4409c2ae016bb0dc04933646478039
jedzer <jenyamalina@tut.by>
2018-05-17 21:45:59 +0300

    Added bunch of stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import re
 import constants
+import random
 
 # import requests
 # def sendMessage(id, text):
@@ -12,11 +13,19 @@ print(bot.get_me())
 # bot.send_message(103416615, "Your mom gay!")
 
 
-
-def SearchForMomGay(msg):
+def searchForMomGay(msg):
     for i in range(11):
         if re.search(constants.YourMomGayArray[i], msg, re.IGNORECASE):
             return True
+
+
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+    markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    markup.row('/bsuirschedule')
+    markup.row('/reddit', '/meme')
+    markup.row('/flipcoin', '/help', '/stop')
+    bot.send_message(message.chat.id, "Welcome!", reply_markup=markup)
 
 
 @bot.message_handler(commands=['changelog'])
@@ -24,11 +33,29 @@ def handle_text(message):
     bot.send_message(message.chat.id, constants.changeLogg)
 
 
+@bot.message_handler(commands=['flipcoin'])
+def handle_text(message):
+    so = random.randint(0, 1)
+    if so == 0:
+        bot.send_message(message.chat.id, constants.heads)
+    else:
+        bot.send_message(message.chat.id, constants.tails)
+
+@bot.message_handler(commands=['help'])
+def handle_text(message):
+    bot.send_message(message.chat.id, constants.help)
+
+
+@bot.message_handler(commands=['stop'])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Goodbye 😢. It was pleasure to help you.", reply_markup=None)
+
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if re.search("no u", message.text, re.IGNORECASE):
         bot.send_message(message.chat.id, "...")
-    elif SearchForMomGay(message.text):
+    elif searchForMomGay(message.text):
         for i in range(11):
             if re.search(constants.YourMomGayArray[i], message.text, re.IGNORECASE):
                 if i == 10:
@@ -38,7 +65,7 @@ def handle_text(message):
     elif re.search("u gay", message.text, re.IGNORECASE):
         bot.send_message(message.chat.id, "NO U!")
     else:
-        bot.send_message(message.chat.id, "Sorry, no such command!")
+        bot.send_message(message.chat.id, "Sorry🙈, no such command!")
 
 
 bot.polling(none_stop=True, interval=0)
