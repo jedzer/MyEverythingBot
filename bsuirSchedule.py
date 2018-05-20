e8aeb460fbb5adfbb847663cc9294ec7e41c0326
jedzer <jenyamalina@tut.by>
2018-05-20 18:00:31 +0300

    Planned BSUIR schedule structure

---
+++ bsuirSchedule.py
@@ -0,0 +1,33 @@
+usersGroupNumber = {}
+
+
+def getFileFromAPI(url):
+    pass
+
+
+def setUserGroupNumber(userId, group):
+    pass
+
+
+def getUserGroupNumber(userId):
+    pass
+
+
+def checkIfGroupExists(groupNumber):
+    pass
+
+
+def getWeek():
+    pass
+
+
+def getOneDaySchedule(userId):
+    pass
+
+
+def getOneWeekSchedule(userId, week):
+    pass
+
+
+def getSchedule(userId):
+    pass
